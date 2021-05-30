fedb8e4babe6181492c30588d2151d9abf8c9d8e
teagaal <smaurenza@disroot.org>
2021-05-30 18:01:23 -0300

    corregir imágenes

--- salamagritte.js
+++ salamagritte.js
@@ -5,12 +5,12 @@ function DelayloadingImages() {
   var imgDiv4 = document.getElementById("imagen4");
   var imgDiv5 = document.getElementById("imagen5");
   var imgDiv6 = document.getElementById("imagen6");
-  imgDiv1.innerHTML = "<img class='img' src='/magritte/1-1.jpeg' />";
-  imgDiv2.innerHTML = "<img class='img' src='/magritte/2-1.jpeg' />";
-  imgDiv3.innerHTML = "<img class='img' src='/magritte/3-1.jpeg' />";
-  imgDiv4.innerHTML = "<img class='img' src='/magritte/4-1.jpeg' />";
-  imgDiv5.innerHTML = "<img class='img' src='/magritte/5-1.jpeg' />";
-  imgDiv6.innerHTML = "<img class='img' src='/magritte/6-1.jpeg' />";
+  imgDiv1.innerHTML = "<img class='img' src='magritte/1-1.jpeg' />";
+  imgDiv2.innerHTML = "<img class='img' src='magritte/2-1.jpeg' />";
+  imgDiv3.innerHTML = "<img class='img' src='magritte/3-1.jpeg' />";
+  imgDiv4.innerHTML = "<img class='img' src='magritte/4-1.jpeg' />";
+  imgDiv5.innerHTML = "<img class='img' src='magritte/5-1.jpeg' />";
+  imgDiv6.innerHTML = "<img class='img' src='magritte/6-1.jpeg' />";
 }
 setTimeout("DelayloadingImages()", 3000);
 
@@ -21,11 +21,11 @@ function DelayloadingImages2() {
   var imgDiv4 = document.getElementById("imagen4");
   var imgDiv5 = document.getElementById("imagen5");
   var imgDiv6 = document.getElementById("imagen6");
-  imgDiv1.innerHTML = "<img class='img' src='/magritte/1-2.jpeg' />";
-  imgDiv2.innerHTML = "<img class='img' src='/magritte/2-2.jpeg' />";
-  imgDiv3.innerHTML = "<img class='img' src='/magritte/3-2.jpeg' />";
-  imgDiv4.innerHTML = "<img class='img' src='/magritte/4-2.jpeg' />";
-  imgDiv5.innerHTML = "<img class='img' src='/magritte/5-2.jpeg' />";
-  imgDiv6.innerHTML = "<img class='img' src='/magritte/6-2.jpeg' />";
+  imgDiv1.innerHTML = "<img class='img' src='magritte/1-2.jpeg' />";
+  imgDiv2.innerHTML = "<img class='img' src='magritte/2-2.jpeg' />";
+  imgDiv3.innerHTML = "<img class='img' src='magritte/3-2.jpeg' />";
+  imgDiv4.innerHTML = "<img class='img' src='magritte/4-2.jpeg' />";
+  imgDiv5.innerHTML = "<img class='img' src='magritte/5-2.jpeg' />";
+  imgDiv6.innerHTML = "<img class='img' src='magritte/6-2.jpeg' />";
 }
 setTimeout("DelayloadingImages2()", 6000);
